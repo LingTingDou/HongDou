ac0f48494ba7cf6a207e4bd2a4935a93d0b22ffd
HongDou <948616667@qq.com>
2017-03-22 23:14:21 +0800

    Music 列表，用到EF(dbfirst)，bootstrap

---
+++ HongDouWeb/Controllers/MusicController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using HongDouWeb.DAL;
+
+namespace HongDouWeb.Controllers
+{
+    public class MusicController : Controller
+    {
+        MusicService ms = new MusicService();
+        // GET: Music
+        public ActionResult Index()
+        {
+            return View(ms.SelectList());
+        }
+
+        
+
+    }
+}
---
+++ HongDouWeb/DAL/MusicService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+
+namespace HongDouWeb.DAL
+{
+    public class MusicService
+    {
+        MusicEntities db = new MusicEntities();
+        public IQueryable<Music_List> SelectList()
+        {
+            return db.Music_List.Where(p=>p.Status==1);
+        }
+    }
+}
