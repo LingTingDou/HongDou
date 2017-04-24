0ac553888dcffc9712be4e6b4a3439037e33189a
BeanLv <lb-drfto@163.com>
2017-04-24 18:50:34 +0800

    DataObject

---
+++ HongDou.Infrasturcture.Data/BaseDataObject.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HongDou.Infrasturcture.Data
+{
+    public abstract partial class BaseDataObject<T> : IEquatable<BaseDataObject<T>>
+    {
+        public T ID { get; set; }
+
+        public virtual bool Equals(BaseDataObject<T> other)
+        {
+            if (ReferenceEquals(other, null))
+                return false;
+
+            return EqualityComparer<T>.Default.Equals(this.ID, other.ID);
+        }
+
+        public sealed override bool Equals(object obj)
+        {
+            return this.Equals(obj as BaseDataObject<T>);
+        }
+
+        public override int GetHashCode()
+        {
+            if (default(T) == null && this.ID == null)
+                return base.GetHashCode();
+
+            return this.ID.GetHashCode();
+        }
+
+        public static bool operator ==(BaseDataObject<T> x, BaseDataObject<T> y)
+        {
+            if (ReferenceEquals(x, y))
+                return true;
+
+            if (ReferenceEquals(x, null) || ReferenceEquals(y, null))
+                return false;
+
+            return x.Equals(y);
+        }
+
+
+        public static bool operator !=(BaseDataObject<T> x, BaseDataObject<T> y)
+        {
+            return !(x == y);
+        }
+    }
+}
---
+++ HongDou.Infrasturcture.Data/IntIDDataObject.cs
@@ -0,0 +1,18 @@
+﻿namespace HongDou.Infrasturcture.Data
+{
+    public abstract partial class IntIDDataObject : BaseDataObject<int>
+    {
+        public override bool Equals(BaseDataObject<int> other)
+        {
+            if (ReferenceEquals(other, null))
+                return false;
+
+            return this.ID == other.ID;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.ID.GetHashCode();
+        }
+    }
+}
